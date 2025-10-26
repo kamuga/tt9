740d965835d46011e0baecfaba74d6521f5c29f2
sspanak <doftor.livain@gmail.com>
2025-10-26 20:23:43 +0200

    colors: migrate stuff from premium

--- app/src/main/java/io/github/sspanak/tt9/ime/UiHandler.java
+++ app/src/main/java/io/github/sspanak/tt9/ime/UiHandler.java
@@ -36,8 +36,8 @@ abstract class UiHandler extends AbstractHandler {
 	protected void initTray() {
 		mainView.getView();
 		createSuggestionBar();
-		getSuggestionOps().setDarkTheme();
-		statusBar = new StatusBar(this, settings, mainView.getView(), this::resetStatus);
+		getSuggestionOps().setColorScheme();
+		statusBar = new StatusBar(this, settings, mainView.getView(), this::resetStatus).setColorScheme();
 	}
 
 
@@ -45,10 +45,10 @@ abstract class UiHandler extends AbstractHandler {
 		if (mainView.create()) {
 			initTray();
 		} else {
-			getSuggestionOps().setDarkTheme();
+			getSuggestionOps().setColorScheme();
 		}
 		setStatusIcon(inputMode, getFinalContext().getLanguage());
-		statusBar.setText(inputMode);
+		statusBar.setColorScheme().setText(inputMode);
 		mainView.showKeyboard();
 		mainView.render();
 
